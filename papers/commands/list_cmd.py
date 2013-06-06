f0750b03885d34aa95360abffcabdf436f7a3420
Olivier Mangin <olivier.mangin@inria.fr>
2013-06-06 12:01:10 +0200

    Adds tags display to list.

--- papers/commands/list_cmd.py
+++ papers/commands/list_cmd.py
@@ -12,9 +12,10 @@ def command(config, ui):
     articles = []
     for n, p in enumerate(rp.all_papers()):
         bibdesc = pretty.bib_oneliner(p.bibentry, color=ui.color)
-        articles.append((u'{num:d}: [{citekey}] {descr}'.format(
+        articles.append((u'{num:d}: [{citekey}] {descr} {labels}'.format(
             num=int(n),
             citekey=ui.colored(rp.citekeys[n], 'purple'),
             descr=bibdesc,
+            labels=ui.colored(''.join(p.metadata['labels']), 'purple'),
             )).encode('utf-8'))
     ui.print_('\n'.join(articles))
