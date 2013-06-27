2981d6d9cc8d7f3c2a93abf835edb3e230c71995
Fabien Benureau <fabien.benureau+git@gmail.com>
2013-06-27 15:17:03 +0200

    websearch accepts multiple keyword

--- papers/commands/websearch_cmd.py
+++ papers/commands/websearch_cmd.py
@@ -5,12 +5,13 @@ import urllib
 def parser(subparsers, config):
     parser = subparsers.add_parser('websearch',
                                    help="launch a search on Google Scholar")
-    parser.add_argument("search_string",
+    parser.add_argument("search_string", nargs = '*',
                         help="the search query (anything googly is possible)")
     return parser
 
 
 def command(config, ui, search_string):
+    print search_string
     url = ("https://scholar.google.fr/scholar?q=%s&lr="
-           % (urllib.quote_plus(search_string)))
+           % (urllib.quote_plus(' '.join(search_string))))
     webbrowser.open(url)
