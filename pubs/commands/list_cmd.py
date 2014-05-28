pubs/pubs

commit d3736e257be2e184e9b8140502803aebf653a92f

moving from pybtex to bibtextparser, fixed major regressions.
* only bibtex format is supported
* all tests except test_repo.py and edit test pass
* edit and update commands were not updated
* removed --format argument from export, only bibtex is supported.

pubs/commands/list_cmd.py
@@ -1,8 +1,8 @@
 from .. import repo
 from .. import pretty
+from .. import bibstruct
 from ..configs import config
 from ..uis import get_ui
-
 
 class InvalidQuery(ValueError):
     pass
@@ -56,20 +56,15 @@ def _get_field_value(query_block):
     return (field, value)
 
 
-def _lower(string, lower=True):
-    if lower:
-        return string.lower()
-    else:
-        return string
-
+def _lower(s, lower=True):
+    return s.lower() if lower else s
 
 def _check_author_match(paper, query, case_sensitive=False):
     """Only checks within last names."""
-    if not 'author' in paper.bibentry.persons:
+    if not 'author' in paper.bibentry:
         return False
-    return any([query in _lower(name, lower=(not case_sensitive))
-                for p in paper.bibentry.persons['author']
-                for name in p.last()])
+    return any([query == _lower(bibstruct.author_last(p), lower=(not case_sensitive))
+                for p in paper.bibentry['author']])
 
 
 def _check_tag_match(paper, query, case_sensitive=False):
@@ -78,7 +73,7 @@ def _check_tag_match(paper, query, case_sensitive=False):
 
 
 def _check_field_match(paper, field, query, case_sensitive=False):
-    return query in _lower(paper.bibentry.fields[field],
+    return query in _lower(paper.bibentry[field],
                        lower=(not case_sensitive))
 
 
@@ -92,7 +87,7 @@ def _check_query_block(paper, query_block, case_sensitive=None):
         return _check_tag_match(paper, value, case_sensitive=case_sensitive)
     elif field == 'author':
         return _check_author_match(paper, value, case_sensitive=case_sensitive)
-    elif field in paper.bibentry.fields:
+    elif field in paper.bibentry:
         return _check_field_match(paper, field, value,
                                   case_sensitive=case_sensitive)
     else:

.ackrc
@@ -1,1 +1,2 @@
 --ignore-directory=is:build
+--ignore-directory=is:pubs.egg-info

NOTES
@@ -14,13 +14,12 @@ A paper correspond to 3 files :
 
 About strings:
 --------------
-- pybtex seems to store entries as utf-8 (TODO: check)
 - so assumption is made that everything is utf-8
 - conversions are performed at print time
 
 Config values:
 --------------
-[papers]
+[pubs]
 open-cmd = open
 edit-cmd = edit
 import-copy = True

pubs/bibstruct.py
@@ -21,19 +21,24 @@ def check_citekey(citekey):
         raise ValueError("Invalid citekey: %s" % citekey)
 
 def verify_bibdata(bibdata):
-    if not hasattr(bibdata, 'entries') or len(bibdata.entries) == 0:
-        raise ValueError('no entries in the bibdata.')
-    if len(bibdata.entries) > 1:
+    if bibdata is None or len(bibdata) == 0:
+        raise ValueError('no valid bibdata')
+    if len(bibdata) > 1:
         raise ValueError('ambiguous: multiple entries in the bibdata.')
 
 def get_entry(bibdata):
     verify_bibdata(bibdata)
-    return bibdata.entries.iteritems().next()
+    for e in bibdata.items():
+        return e
 
 def extract_citekey(bibdata):
     verify_bibdata(bibdata)
     citekey, entry = get_entry(bibdata)
     return citekey
+
+def author_last(author_str):
+    """ Return the last name of the author """
+    return author_str.split(',')[0]
 
 def generate_citekey(bibdata):
     """ Generate a citekey from bib_data.
@@ -44,17 +49,17 @@ def generate_citekey(bibdata):
     """
     citekey, entry = get_entry(bibdata)
 
-    author_key = 'author' if 'author' in entry.persons else 'editor'
+    author_key = 'author' if 'author' in entry else 'editor'
     try:
-        first_author = entry.persons[author_key][0]
+        first_author = entry[author_key][0]
     except KeyError:
         raise ValueError(
                 'No author or editor defined: cannot generate a citekey.')
     try:
-        year = entry.fields['year']
+        year = entry['year']
     except KeyError:
         year = ''
-    citekey = u'{}{}'.format(u''.join(first_author.last()), year)
+    citekey = u'{}{}'.format(u''.join(author_last(first_author)), year)
 
     return str2citekey(citekey)
 
@@ -67,21 +72,21 @@ def extract_docfile(bibdata, remove=False):
     citekey, entry = get_entry(bibdata)
 
     try:
-        if 'file' in entry.fields:
-            field = entry.fields['file']
+        if 'file' in entry:
+            field = entry['file']
             # Check if this is mendeley specific
             for f in field.split(':'):
                 if len(f) > 0:
                     break
             if remove:
-                entry.fields.pop('file')
+                entry.pop('file')
             # This is a hck for Mendeley. Make clean
             if f[0] != '/':
                 f = '/' + f
             return f
-        if 'attachments' in entry.fields:
-            return entry.fields['attachments']
-        if 'pdf' in entry.fields:
-            return entry.fields['pdf']
+        if 'attachments' in entry:
+            return entry['attachments']
+        if 'pdf' in entry:
+            return entry['pdf']
     except (KeyError, IndexError):
         return None

pubs/commands/add_cmd.py
@@ -108,7 +108,7 @@ def command(args):
         if copy_doc is None:
             copy_doc = config().import_copy
         if copy_doc:
-            docfile = rp.databroker.copy_doc(citekey, docfile)
+            docfile = rp.databroker.add_doc(citekey, docfile)
 
     # create the paper
 

pubs/commands/attach_cmd.py
@@ -34,7 +34,7 @@ def command(args):
     try:
         document = args.document
         if copy:
-            document = rp.databroker.copy_doc(paper.citekey, document)
+            document = rp.databroker.add_doc(paper.citekey, document)
         else:
             pass # TODO warn if file does not exists
         paper.docpath = document

pubs/commands/edit_cmd.py
@@ -15,6 +15,24 @@ def parser(subparsers):
     return parser
 
 
+def edit_meta(citekey):
+    rp = repo.Repository(config())
+    coder = endecoder.EnDecoder()
+    filepath = os.path.join(rp.databroker.databroker.filebroker.metadir(), citekey+'.yaml')
+    with open(filepath) as f:
+        content = f.read()
+
+
+
+def edit_bib(citekey):
+    rp = repo.Repository(config())
+    coder = endecoder.EnDecoder()
+    filepath = os.path.join(rp.databroker.databroker.filebroker.bibdir(), citekey+'.bib')
+    with open(filepath) as f:
+        content = f.read()
+
+
+
 def command(args):
 
     ui = get_ui()
@@ -26,8 +44,7 @@ def command(args):
     if meta:
         filepath = os.path.join(rp.databroker.databroker.filebroker.metadir(), citekey+'.yaml')
     else:
-        filepath = os.path.join(rp.databroker.databroker.filebroker.bibdir(), citekey+'.bibyaml')
-
+        filepath = os.path.join(rp.databroker.databroker.filebroker.bibdir(), citekey+'.bib')
 
     with open(filepath) as f:
         content = f.read()

commit 47103058d658a0aa168aa3603ee339fedc078fee

FIX bug in query introduced by d3736e25.
Author name equlity to query is tested instead of query inclusion.

pubs/commands/list_cmd.py
@@ -69,7 +69,7 @@ def _check_author_match(paper, query, case_sensitive=False):
     """Only checks within last names."""
     if not 'author' in paper.bibentry:
         return False
-    return any([query == _lower(bibstruct.author_last(p), lower=(not case_sensitive))
+    return any([query in _lower(bibstruct.author_last(p), lower=(not case_sensitive))
                 for p in paper.bibentry['author']])
 
 