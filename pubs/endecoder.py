pubs/pubs

commit 0b64bf086ed11d1b40e9454db76bb4417eee73a3

Update for latest bibtexparser (>= 0.5.3).
Breaks compatibility with previous versions. Also fixes the UnicodeWarning.

pubs/endecoder.py
@@ -1,6 +1,5 @@
 from __future__ import print_function, absolute_import, division, unicode_literals
 
-import io
 import copy
 
 try:
@@ -11,7 +10,6 @@ except ImportError:
     exit(-1)
 
 import yaml
-
 
 
 """Important notice:
@@ -44,7 +42,9 @@ def customizations(record):
 
     return record
 
-bibfield_order = ['author', 'title', 'journal', 'institution', 'publisher', 'year', 'month', 'number', 'pages', 'link', 'doi', 'id', 'note', 'abstract']
+bibfield_order = ['author', 'title', 'journal', 'institution', 'publisher',
+                  'year', 'month', 'number', 'pages', 'link', 'doi', 'id',
+                  'note', 'abstract']
 
 
 class EnDecoder(object):
@@ -60,7 +60,7 @@ class EnDecoder(object):
 
     def encode_metadata(self, metadata):
         return yaml.safe_dump(metadata, allow_unicode=True,
-                              encoding=None, indent = 4)
+                              encoding=None, indent=4)
 
     def decode_metadata(self, metadata_raw):
         return yaml.safe_load(metadata_raw)
@@ -99,14 +99,20 @@ class EnDecoder(object):
         bibraw += '}\n'
         return bibraw
 
-    def decode_bibdata(self, bibdata_raw):
+    def decode_bibdata(self, bibdata_unicode):
         """"""
-        bibdata_stream = io.StringIO(bibdata_raw)
-        return self._decode_bibdata(bibdata_stream)
+        if isinstance(bibdata_unicode, str):
+            # Nothing to do for python3
+            bibdata_string = bibdata_unicode
+        else:
+            # For python2 bibtexparser expects utf8 encoded string
+            bibdata_string = bibdata_unicode.encode('utf8')
+        return self._decode_bibdata(bibdata_string)
 
-    def _decode_bibdata(self, bibdata_stream):
+    def _decode_bibdata(self, bibdata_string):
         try:
-            entries = bp.bparser.BibTexParser(bibdata_stream, customization=customizations).get_entry_dict()
+            entries = bp.bparser.BibTexParser(
+                bibdata_string, customization=customizations).get_entry_dict()
             # Remove 'id' attribute which is stored as citekey
             for e in entries:
                 entries[e].pop('id')

commit 921c54f5356df93f8e605c996b9c90bffb7e874a

Closes issue #21.
0b64bf0 introduced a hack (http://git.io/1ZbMkg) related to an issue
in bibtexparser : sciunto/python-bibtexparser/issues/17.
This has been fixed by sciunto/python-bibtexparser@6beb646 and the hack
can be safely removed.

Pubs should be updated accordingly as soon as the bibtexparser FIX gets to a released version.

pubs/endecoder.py
@@ -1,4 +1,5 @@
-from __future__ import print_function, absolute_import, division, unicode_literals
+from __future__ import (print_function, absolute_import, division,
+                        unicode_literals)
 
 import copy
 
@@ -92,27 +93,20 @@ class EnDecoder(object):
         for key in bibfield_order:
             if key in bibentry:
                 value = bibentry.pop(key)
-                bibraw += '    {} = {{{}}},\n'.format(key, EnDecoder._encode_field(key, value))
+                bibraw += '    {} = {{{}}},\n'.format(
+                    key, EnDecoder._encode_field(key, value))
         for key, value in bibentry.items():
             if key != 'type':
-                bibraw += '    {} = {{{}}},\n'.format(key, EnDecoder._encode_field(key, value))
+                bibraw += '    {} = {{{}}},\n'.format(
+                    key, EnDecoder._encode_field(key, value))
         bibraw += '}\n'
         return bibraw
 
-    def decode_bibdata(self, bibdata_unicode):
+    def decode_bibdata(self, bibdata):
         """"""
-        if isinstance(bibdata_unicode, str):
-            # Nothing to do for python3
-            bibdata_string = bibdata_unicode
-        else:
-            # For python2 bibtexparser expects utf8 encoded string
-            bibdata_string = bibdata_unicode.encode('utf8')
-        return self._decode_bibdata(bibdata_string)
-
-    def _decode_bibdata(self, bibdata_string):
         try:
             entries = bp.bparser.BibTexParser(
-                bibdata_string, customization=customizations).get_entry_dict()
+                bibdata, customization=customizations).get_entry_dict()
             # Remove 'id' attribute which is stored as citekey
             for e in entries:
                 entries[e].pop('id')