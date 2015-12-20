0ab831008939a6eb62fe4e342941140d14f4d9e8
Fabien Benureau <fabien.benureau+git@gmail.com>
2015-12-20 18:43:22 +0100

    Fix color in test_oneliner

--- tests/test_pretty.py
+++ tests/test_pretty.py
@@ -20,14 +20,14 @@ class TestPretty(unittest.TestCase):
         decoder = endecoder.EnDecoder()
         bibdata = decoder.decode_bibdata(bibtex_raw0)
         line = u'Page, Lawrence et al. "The PageRank Citation Ranking: Bringing Order to the Web." (1999)'
-        self.assertEqual(pretty.bib_oneliner(bibdata['Page99']), line)
+        self.assertEqual(color.undye(pretty.bib_oneliner(bibdata['Page99'])), line)
 
     def test_oneliner_no_year(self):
         decoder = endecoder.EnDecoder()
         bibdata = decoder.decode_bibdata(bibtex_raw0)
         bibdata['Page99'].pop('year')
         line = u'Page, Lawrence et al. "The PageRank Citation Ranking: Bringing Order to the Web."'
-        self.assertEqual(pretty.bib_oneliner(bibdata['Page99']), line)
+        self.assertEqual(color.undye(pretty.bib_oneliner(bibdata['Page99'])), line)
 
 if __name__ == '__main__':
     unittest.main()
