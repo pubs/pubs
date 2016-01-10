pubs/pubs

commit 666941d471dbd33ff2b7a55fd28ef5c29d9bd517

Set bibtexparser homogenize_fields to True
The default changed from True to False. It is not explicitly set.

pubs/endecoder.py
@@ -38,6 +38,7 @@ def customizations(record):
         :param record: a record
         :returns: -- customized record
     """
+
     record = bp.customization.convert_to_unicode(record)
     record = bp.customization.type(record)
     record = bp.customization.author(record)
@@ -115,7 +116,8 @@ class EnDecoder(object):
         """"""
         try:
             entries = bp.bparser.BibTexParser(
-                bibdata, customization=customizations).get_entry_dict()
+                bibdata, homogenize_fields=True,
+                customization=customizations).get_entry_dict()
             # Remove id from bibtexparser attribute which is stored as citekey
             for e in entries:
                 entries[e].pop(BP_ID_KEY)

commit a5a653becd761bf56a7ac38e309c29a5ea3307a3

Support old bibtexparser keywords
So previous commit 666941d471 broke the current pip version
of bibtexparser (0.6.2), which does not have a
homegeneize_fields keyword. My bad and fixed!

pubs/endecoder.py
@@ -115,9 +115,15 @@ class EnDecoder(object):
     def decode_bibdata(self, bibdata):
         """"""
         try:
-            entries = bp.bparser.BibTexParser(
-                bibdata, homogenize_fields=True,
-                customization=customizations).get_entry_dict()
+            try:
+                entries = bp.bparser.BibTexParser(
+                    bibdata, homogenize_fields=True,
+                    customization=customizations).get_entry_dict()
+            except TypeError:
+                entries = bp.bparser.BibTexParser(
+                    bibdata,
+                    customization=customizations).get_entry_dict()
+
             # Remove id from bibtexparser attribute which is stored as citekey
             for e in entries:
                 entries[e].pop(BP_ID_KEY)