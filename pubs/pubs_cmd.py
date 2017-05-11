pubs/pubs

commit 5ca090668febbb984745f026a4ecd4e9d67d8592

Handle IOError: [Errno 32] Broken pipe
Will happen if piping through a pager like less, that closes
stdout before everything has been printed.

Related: #47

pubs/pubs
@@ -1,5 +1,19 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
+import sys
+
 from pubs import pubs_cmd
-pubs_cmd.execute()
+
+try:
+    pubs_cmd.execute()
+except IOError:
+    # stdout or stderr has been closed, cleaning up
+    try:
+        sys.stdout.close()
+    except IOError:
+        pass
+    try:
+        sys.stderr.close()
+    except IOError:
+        pass

commit a02f67f133a535e7c61172b1e81fd26660b164ae

Minor improvement of error handling.
Also removes ignore of broken pipe from 5ca090668 since this is not
handling it the right way (see #60).

pubs/pubs
@@ -1,19 +1,7 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 
-import sys
-
 from pubs import pubs_cmd
 
-try:
-    pubs_cmd.execute()
-except BrokenPipeError:
-    # stdout or stderr has been closed, cleaning up
-    try:
-        sys.stdout.close()
-    except IOError:
-        pass
-    try:
-        sys.stderr.close()
-    except IOError:
-        pass
+
+pubs_cmd.execute()

pubs/pubs_cmd.py
@@ -84,4 +84,5 @@ def execute(raw_args=sys.argv):
         args.func(conf, args)
 
     except Exception as e:
-        uis.get_ui().handle_exception(e)
+        if not uis.get_ui().handle_exception(e):
+            raise

pubs/uis.py
@@ -84,11 +84,14 @@ class PrintUI(object):
         sys.exit(error_code)
 
     def handle_exception(self, exc):
-        if self.debug:
-            raise
-        else:
+        """Attempts to handle exception.
+
+        :returns: True if exception has been handled (currently never happens)
+        """
+        if not self.debug:
             self.error(ustr(exc))
             self.exit()
+        return False
 
 
 class InputUI(PrintUI):