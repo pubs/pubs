7956e9bcb28b092b359ea20357043e4e69ea0dbc
Olivier Mangin <olivier@mangin.com>
2018-01-23 16:19:31 -0500

    Fixes wrong option for extras_require and cleanup.

--- setup.py
+++ setup.py
@@ -1,30 +1,32 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 VERSION = '0.7.0'
 
 setup(
-    name         = 'pubs',
-    version      = VERSION,
-    author       = 'Fabien Benureau, Olivier Mangin, Jonathan Grizou',
-    author_email = 'fabien.benureau@gmail.com',
-    maintainer   = 'Olivier Mangin',
-    url          = 'https://github.com/pubs/pubs',
+    name='pubs',
+    version=VERSION,
+    author='Fabien Benureau, Olivier Mangin, Jonathan Grizou',
+    author_email='fabien.benureau@gmail.com',
+    maintainer='Olivier Mangin',
+    url='https://github.com/pubs/pubs',
 
-    description  = 'command-line scientific bibliography manager',
-    packages     = ['pubs', 'pubs.config',
-                            'pubs.commands',
-                            'pubs.templates',
-                            'pubs.plugs', 'pubs.plugs.alias'],
-    scripts      = ['pubs/pubs'],
+    description='command-line scientific bibliography manager',
+    packages=['pubs',
+              'pubs.config',
+              'pubs.commands',
+              'pubs.templates',
+              'pubs.plugs',
+              'pubs.plugs.alias'],
+    scripts=['pubs/pubs'],
 
-    install_requires = ['pyyaml', 'bibtexparser', 'python-dateutil', 'requests',
-                        'configobj',
-                        'beautifulsoup4'], # to be made optional?
-    tests_require = ['pyfakefs>=2.7'],
-    extra_require = {'autocompletion': ['argcomplete'],
-                     },
+    install_requires=['pyyaml', 'bibtexparser', 'python-dateutil', 'requests',
+                      'configobj',
+                      'beautifulsoup4'],  # to be made optional?
+    tests_require=['pyfakefs>=2.7'],
+    extras_require={'autocompletion': ['argcomplete'],
+                    },
 
     classifiers=[
         'Development Status :: 4 - Beta',
