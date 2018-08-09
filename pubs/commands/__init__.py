7dae35e722d672bfc93892403fecee8dc57d5196
Olivier Mangin <olivier@mangin.com>
2018-08-09 20:54:44 +0200

    Adds the statistics command. (Fixes #8)

--- pubs/commands/__init__.py
+++ pubs/commands/__init__.py
@@ -6,9 +6,11 @@ from . import add_cmd
 from . import rename_cmd
 from . import remove_cmd
 from . import list_cmd
+from . import edit_cmd
+from . import tag_cmd
+from . import statistics_cmd
 # doc
 from . import doc_cmd
-from . import tag_cmd
 from . import note_cmd
 # bulk
 from . import export_cmd
@@ -16,5 +18,3 @@ from . import import_cmd
 # bonus
 from . import websearch_cmd
 from . import url_cmd
-
-from . import edit_cmd
