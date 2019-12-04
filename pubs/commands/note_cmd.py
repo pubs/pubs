25e1f946a331cba6671ae84e3aa1de8e95593b97
dvklopfenstein <dvklopfenstein@users.noreply.github.com>
2019-12-04 03:22:20 -0500

    Add note echo arg

--- pubs/commands/note_cmd.py
+++ pubs/commands/note_cmd.py
@@ -1,9 +1,14 @@
+from __future__ import unicode_literals
+
+import os
+from .. import p3
 from .. import repo
 from ..uis import get_ui
 from ..utils import resolve_citekey
 from ..completion import CiteKeyCompletion
 from ..events import NoteEvent
 from ..content import write_file
+from ..content import read_text_file
 
 
 def parser(subparsers, conf):
@@ -13,6 +18,8 @@ def parser(subparsers, conf):
                         ).completer = CiteKeyCompletion(conf)
     parser.add_argument('-a', '--append',
                         help='append a line of text to the notes file', default=None)
+    parser.add_argument('-e', '--echo', action='store_true',
+                        help='echo the contents of the notes file')
     return parser
 
 
@@ -22,10 +29,13 @@ def command(conf, args):
     rp = repo.Repository(conf)
     citekey = resolve_citekey(rp, args.citekey, ui=ui, exit_on_fail=True)
     notepath = rp.databroker.real_notepath(citekey, rp.conf['main']['note_extension'])
-    if args.append is None:
+    print('NNNNNNNNNNNNN sitepath pubs note', notepath, args)
+    if args.append is None and not args.echo:
         ui.edit_file(notepath, temporary=False)
-    else:
-        latestnote = '{txt}\n'.format(txt=args.append)
+    elif args.append:
+        latestnote = '{txt}\n'.format(txt=p3.u_maybe(args.append))
         write_file(notepath, latestnote, 'a')
+    elif args.echo and os.path.exists(notepath):
+        print(read_text_file(notepath, fail=False))
     NoteEvent(citekey).send()
     rp.close()
