e73652e65e16c7f88a0f1c64cdf27bce95fd0c17
Fabien C. Y. Benureau <fabien@benureau.com>
2021-01-16 15:57:00 +0900

    print_msg function for pybtex

--- pubs/endecoder.py
+++ pubs/endecoder.py
@@ -38,6 +38,57 @@ BIBFIELD_ORDER = ['author', 'title', 'journal', 'institution', 'publisher',
                   'doi', 'note', 'abstract']
 
 
+
+class BibDecodingError(Exception):
+
+    def __init__(self, error_msg, bibdata):
+        """
+        :param error_msg: specific message about what went wrong
+        :param bibdata:   the data that was unsuccessfully decoded.
+        """
+        super(Exception, self).__init__(error_msg)  # make `str(self)` work.
+        self.data = bibdata
+
+
+class EmptyData(BibDecodingError):
+
+    def __init__(self, bibstr, msg):
+        self.bibstr, self.msg = bibstr, msg
+
+    def print(self, ui):
+        ui.error(self.msg)
+
+class SyntaxError(BibDecodingError):
+
+    def __init__(self, bibstr, pybtex_exc):
+        self.bibstr, self.pybtex_exc = bibstr, pybtex_exc
+
+    def print(self, ui, context_margin=3):
+        """
+        Print the syntax error.
+
+        :param context_margin:  how much line to display around the problematic line
+        """
+        exc = self.pybtex_exc
+        lines = self.bibstr.split('\n')
+        ui.error('bibtex syntax error at line {}: {}'.format(exc.lineno, exc.args[0]))
+
+        n_digit = math.floor(math.log10(len(lines))+1)
+        line_prefix = 'L{:' + str(n_digit) + 'd}: {}'
+        for i, line in enumerate(lines[max(0, exc.lineno-context_margin-1): exc.lineno-1]):
+            lineno = max(0, exc.lineno-context_margin) + i
+            ui.message(line_prefix.format(lineno, line))
+        context_lines = e.get_context().split('\n')
+        for i, line in enumerate(context_lines):
+            if i == 0:
+                ui.message(line_prefix.format(exc.lineno, line))
+            else:
+                ui.message(' ' + n_digit*'.' + '   ' + line)
+        for i, line in enumerate(lines[e.lineno+1:min(len(lines), e.lineno + context_margin + 1)]):
+            lineno = e.lineno + 1 + i
+            ui.message(line_prefix.format(lineno, line))
+
+
 def sanitize_citekey(record):
     record[BP_ID_KEY] = record[BP_ID_KEY].strip('\n')
     return record
@@ -95,70 +146,47 @@ class EnDecoder(object):
 
     def encode_bibdata(self, bibdata, ignore_fields=[]):
         """Encode bibdata """
+        
         bpdata = bp.bibdatabase.BibDatabase()
         bpdata.entries = [self._entry_to_bp_entry(k, copy.copy(bibdata[k]),
                                                   ignore_fields=ignore_fields)
                           for k in bibdata]
         return self.bwriter.write(bpdata)
 
-    def _entry_to_bp_entry(self, key, entry, ignore_fields=[]):
-        """Convert back entries to the format expected by bibtexparser."""
-        entry[BP_ID_KEY] = key
-        # Convert internal 'type' to bibtexparser entrytype key
-        entry[BP_ENTRYTYPE_KEY] = entry.pop(TYPE_KEY)
-        for f in ignore_fields:
-            entry.pop(f, None)
-        if 'author' in entry:
-            entry['author'] = ' and '.join(
-                author for author in entry['author'])
-        if 'editor' in entry:
-            entry['editor'] = ' and '.join(
-                editor for editor in entry['editor'])
-        if 'keyword' in entry:
-            entry['keyword'] = ', '.join(
-                keyword for keyword in entry['keyword'])
-        return entry
+    # def _entry_to_bp_entry(self, key, entry, ignore_fields=[]):
+    #     """Convert back entries to the format expected by bibtexparser."""
+    #     entry[BP_ID_KEY] = key
+    #     # Convert internal 'type' to bibtexparser entrytype key
+    #     entry[BP_ENTRYTYPE_KEY] = entry.pop(TYPE_KEY)
+    #     for f in ignore_fields:
+    #         entry.pop(f, None)
+    #     if 'author' in entry:
+    #         entry['author'] = ' and '.join(
+    #             author for author in entry['author'])
+    #     if 'editor' in entry:
+    #         entry['editor'] = ' and '.join(
+    #             editor for editor in entry['editor'])
+    #     if 'keyword' in entry:
+    #         entry['keyword'] = ', '.join(
+    #             keyword for keyword in entry['keyword'])
+    #     return entry
+
 
     def decode_bibdata(self, bibstr):
         """Decodes bibdata from string.
 
-        If the decoding fails, returns a BibDecodingError.
+        If the decoding fails, returns a BibDecodingError, either EmptyData or SyntaxError.
         """
+        bibstr = bibstr.strip()  # remove leading and trailing spaces
         if len(bibstr) == 0:
-            error_msg = 'parsing error: the provided string has length zero.'
-            raise self.BibDecodingError(error_msg, bibstr)
+            error_msg = 'the provided string has no content.'
+            raise EmtpyData(error_msg, bibstr)
         try:
-            entries = bp.bparser.BibTexParser(
-                bibstr, common_strings=True, customization=customizations,
-                homogenize_fields=True, ignore_nonstandard_types=False).get_entry_dict()
-            # Remove id from bibtexparser attribute which is stored as citekey
-            for e in entries:
-                entries[e].pop(BP_ID_KEY)
-                # Convert bibtexparser entrytype key to internal 'type'
-                t = entries[e].pop(BP_ENTRYTYPE_KEY)
-                entries[e][TYPE_KEY] = t
-                # Temporary fix to #188 (to be fully fixed when the upstream
-                # issue: sciunto-org/python-bibtexparser/#229 is fixed too)
-                if 'editor' in entries[e]:
-                    entries[e]['editor'] = [
-                        editor['name'] if isinstance(editor, dict) else editor
-                        for editor in entries[e]['editor']]
+            entries = pybtex.database.parse_string(bibstr).entries
             if len(entries) > 0:
                 return entries
             else:
-                raise self.BibDecodingError(('no valid entry found in the provided data: '
-                                            ' {}').format(bibstr), bibstr)
-        except (pyparsing.ParseException, pyparsing.ParseSyntaxException) as e:
-            error_msg = self._format_parsing_error(e)
-            raise self.BibDecodingError(error_msg, bibstr)
-        except bibtexparser.bibdatabase.UndefinedString as e:
-            error_msg = 'parsing error: undefined string in provided data: {}'.format(e)
-            raise self.BibDecodingError(error_msg, bibstr)
-
-    @classmethod
-    def _format_parsing_error(cls, e):
-        """Transform a pyparsing exception into an error message
-
-        Does a best effort to be useful, but might need to be improved.
-        """
-        return '{}\n{}^\n{}'.format(e.line, (e.column - 1) * ' ', e)
+                error_msg = 'no valid entry found in the provided data:\n{}'.format(bibstr)
+                raise EmptyData(error_msg, bibstr)
+        except pybtex.parser.SyntaxError as exc:
+            raise SyntaxError(bibstr, exc)
